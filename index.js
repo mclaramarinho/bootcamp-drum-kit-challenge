1189d265a4c4e11b1d857361c23e51fd60a87c33
Clara Marinho <119897667+mclaramarinho@users.noreply.github.com>
2023-02-08 10:30:28 -0300

    Refactored and new visual function
    Added a function to change the key style for 100ms after pressed

--- index.js
+++ index.js
@@ -1,55 +1,18 @@
 (document.querySelectorAll(".drum")).forEach(item => {
     item.addEventListener("click", function (){
-        
         var button = this.innerHTML;
-
-        switch(button){
-            case "w":
-                var tom1 = new Audio('sounds/tom-1.mp3');
-                tom1.play();
-            break;
-
-            case "a":
-                var tom2 = new Audio('sounds/tom-2.mp3');
-                tom2.play();
-            break;
-            
-            case "s":
-                var tom3 = new Audio ('sounds/tom-3.mp3');
-                tom3.play();
-            break;
-
-            case "d":
-                var tom4 = new Audio('sounds/tom-4.mp3');
-                tom4.play();
-            break;
-
-            case "j":
-                var snare = new Audio('sounds/snare.mp3')
-                snare.play();
-            break;
-
-            case "k":
-                var crash = new Audio('sounds/crash.mp3');
-                crash.play();
-            break;
-
-            case "l":
-                var kick = new Audio('sounds/kick-bass.mp3');
-                kick.play();
-            break;
-
-            default:
-                console.log("Button inner html not specified in switch statement")
-        }
-    });
-
+        playSound(button);
+        keyPressed(button);
+    })
 })
 
-
-
 document.addEventListener("keydown", (event) =>{
-    switch(event.key){
+    playSound(event.key);
+    keyPressed(event.key);
+})
+
+function playSound (key){
+    switch(key){
         case "w":
             var tom1 = new Audio('sounds/tom-1.mp3');
             tom1.play();
@@ -84,8 +47,13 @@ document.addEventListener("keydown", (event) =>{
             var kick = new Audio('sounds/kick-bass.mp3');
             kick.play();
         break;
-
-        default:
-            alert("ATTENTION! You can only play with the keyboard keys: W, A, S, D, J, K and L")
+        
     }
-})
+}
+
+function keyPressed (key){
+    document.querySelector("."+key).classList.add("pressed");
+    setTimeout(function(){
+        document.querySelector("."+key).classList.remove("pressed");
+    }, 100);
+}
